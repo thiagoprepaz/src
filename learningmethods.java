62fd96dd0c575fc5b1f11fcdd81f8ae3fb26daf9
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-08-21 14:32:59 -0400

    aprendendo sobre metodos e funções

--- learningmethods.java
+++ learningmethods.java
@@ -1,7 +1,7 @@
 public class learningmethods {
     
 
-    public static int bubble_short_1(int lista){
+    public static int a(int lista){
         /* Aqui é criada uma variavel local de lista que recebe 50,
         valida somente dentro desse escopo 
         */
@@ -11,7 +11,7 @@ public class learningmethods {
     public static void main(String[] args) {
     int lista=40;
 
-        bubble_short_1(lista);
+        a(lista);
         System.out.printf("%d", lista);
         /*porém por mais que a referencia de lista tenha sido alterada, 
         lista, global, continua com seu valor original  */ 
