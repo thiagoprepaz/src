fbcb1f6ed7ef0feedb10d8f7725ce0f871380163
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-08-22 19:23:59 -0400

    incrementando a segunda bubble

--- preprova.java
+++ preprova.java
@@ -20,4 +20,16 @@ public class preprova {
             }
         }
     }
+     public static void bubbleSort2(int[] lista) {
+        int n = lista.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (lista[j] > lista[j + 1]) {
+                    int temp = lista[j];
+                    lista[j] = lista[j + 1];
+                    lista[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
